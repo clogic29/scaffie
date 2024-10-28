dc0cab26335178cecedb8b95d39140860afaa2c5
clogic <choiseungil29@gmail.com>
2024-10-28 16:42:03 +0900

    initial commit

---
+++ scaffolder/__main__.py
@@ -0,0 +1,17 @@
+
+
+test_value = {
+    'app': {"models": [
+      {
+        "name": "QWER",
+        "fields": [
+        ]
+      },
+      {
+        "name": "PINK",
+        "fields": [
+        ]
+      }
+    ]
+    }
+}
